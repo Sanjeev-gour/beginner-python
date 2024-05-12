823944ba985a68b7b463ca13bfebbf306197abc2
Sanjeev-gour <sanjeevgour2004@gmail.com>
2024-05-12 21:58:08 +0530

    pandas 1

--- lists.py/pandas_1_movie-analysis.py
+++ lists.py/pandas_1_movie-analysis.py
@@ -1,5 +1,7 @@
 import csv
 
+#using normal python code 
+'''
 def cal_ratings_stats(data,industry=None):
     ratings = []
     for row in data:
@@ -25,6 +27,46 @@ print(f"All records: Min rating={min_rating}, Max rating = {max_rating}, Average
 
 max_rating, min_rating, avg_rating = cal_ratings_stats(data,industry="Hollywood")
 print(f"All records: Min rating={min_rating}, Max rating = {max_rating}, Average rating={avg_rating}")
+'''
+
+#using pandas library
+
+import pandas as pd
+
+df = pd.read_csv("movies.csv")
+
+#print(df)
+'''
+print(df.head())#will print only few rows from top
+
+print(df.tail(7))#will print only few rows from bottom
+
+print(df.sample())#will print randomly
+
+print(df[2:6])#will print from specific index to specific index
+
+print(df.shape)#number of rows and columns
+'''
+
+#can be printed in both ways
+#print(df["imdb_rating"])#class object
+#print(df.imdb_rating)#property
+
+#print("Minimum rating:",df.imdb_rating.min(),"Maximum rating:",df.imdb_rating.max(),"Average rating:",df.imdb_rating.mean())
+
+
+# if to print ratings for specific industry 
+
+df_b = df[df.industry=="Bollywood"]
+df_h = df[df.industry=="Hollywood"]
+
+print(df_b)
+print("Minimum rating of bollywood:",df_b.imdb_rating.min(),"Maximum rating of bollywood:",df_b.imdb_rating.max(),"Average rating of bollywood:",df_b.imdb_rating.mean())
+
+
+print(df_h)
+print("Minimum rating of Hollywood:",df_h.imdb_rating.min(),"Maximum rating of Hollywood:",df_h.imdb_rating.max(),"Average rating of Hollywood:",df_h.imdb_rating.mean())
+
 
 
 
